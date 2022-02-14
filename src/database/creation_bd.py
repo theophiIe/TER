c7a2b08de1920ae9cda509d64281a4c40e333655
qgruchet <quentin.gruchet78@gmail.com>
2022-02-14 18:42:12 +0100

    Creation des tables dans la base de données

--- src/database/creation_bd.py
+++ src/database/creation_bd.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy_utils import database_exists, create_database
+from table import Base
 
-engine = create_engine("postgresql://postgres@/lessurligneurs")
+engine = create_engine(f"postgresql://postgres@/lessurligneurs")
 print(database_exists(engine.url))
 if not database_exists(engine.url):
     create_database(engine.url)
 
 print(database_exists(engine.url))
+
+session = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
 
 
 if __name__ == '__main__':
