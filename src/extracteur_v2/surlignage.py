355100f76ea1ef35ed2e290dc3ab082cccb8f18b
qgruchet <quentin.gruchet78@gmail.com>
2022-03-28 17:58:34 +0200

    Ajout de plusieurs vérfications + refactoring / ajout de certaines fonctions

--- src/extracteur_v2/surlignage.py
+++ src/extracteur_v2/surlignage.py
@@ -31,25 +31,35 @@ class Surlignage:
         self.auteurs = []
         self.relecteurs = []
         self.redaction = []
-        self.source = []
+        self.url_source = []
+        self.nom_source = []
+        self.references = []
         self.correction = []
         self.contenu = []
         self.meme_theme = []
 
     def get_url(self, page) -> None:
         articles = page.find_all(class_='grid-item')
+
         for article in articles:
             for lien in article.find_all('a'):
                 self.url_surlignage.append(lien.get('href'))
 
     def get_titre_surlignage(self, page) -> None:
         titre = page.find('h1')
-        contenu_titre = titre.text if titre.text != '' else None
+        contenu_titre = None
+
+        if titre is not None:
+            contenu_titre = titre.text if titre.text != '' else None
+
         self.titre.append(contenu_titre)
 
     def get_etiquette_surlignage(self, page) -> None:
         etiquette = page.find(class_='etiquette')
-        contenu_etiquette = etiquette.text if etiquette.text != '' else None
+        contenu_etiquette = None
+
+        if etiquette is not None:
+            contenu_etiquette = etiquette.text if etiquette.text != '' else None
         self.etiquette.append(contenu_etiquette)
 
     def get_meme_theme_surlignage(self, page) -> None:
@@ -63,56 +73,151 @@ class Surlignage:
         self.meme_theme.append(url_meme_theme)
 
     def get_date_surlignage(self, page) -> None:
-        date = page.find(class_='articles-dates')
+        dates = page.find(class_='articles-dates')
+        date_creation = None
+        date_modification = None
 
-        # Ajouter une vérification dans le cas où on n'a pas de date ou juste la date de création
-        if re.search(self.regex_date, date.text):
-            self.date_creation.append(re.findall(self.regex_date, date.text)[0])
-            self.date_modification.append(re.findall(self.regex_date, date.text)[1])
+        if dates is not None:
+            if re.search(self.regex_date, dates.text):
+                date_creation = re.findall(self.regex_date, dates.text)[0]
+                if len(re.findall(self.regex_date, dates.text)) > 1:
+                    date_modification = re.findall(self.regex_date, dates.text)[1]
 
-    def get_contributeur_surlignage(self, page) -> None:
-        contributeurs = page.find(class_='articles-contributeurs').find_all('p')
+        self.date_creation.append(date_creation)
+        self.date_modification.append(date_modification)
 
-        # Ajouter des vérifications + implémenter Flair pour reconnaitre les noms
-        self.auteurs.append(contributeurs[0].text)
-        self.relecteurs.append(contributeurs[1].text)
-        self.redaction.append(contributeurs[2].text)
+    # Pour la V2
+    def get_contributeur_surlignage(self, page) -> bool:
+        articles_contributeurs = page.find(class_='articles-contributeurs')
+
+        if articles_contributeurs is not None:
+            contributeurs = articles_contributeurs.find_all('p')
+
+            if contributeurs:
+
+                auteurs = []
+                relecteurs = []
+                secretariat = []
+
+                for contributeur in contributeurs:
+                    if contributeur.text.startswith('Auteur'):
+                        auteurs.append(contributeur.text)
+                    elif contributeur.text.startswith('Relecteurs'):
+                        relecteurs.append(contributeur.text)
+                    elif contributeur.text.startswith('Secrétariat'):
+                        secretariat.append(contributeur.text)
+
+                self.auteurs.append(auteurs)
+                self.relecteurs.append(relecteurs)
+                self.redaction.append(secretariat)
+                return True
+
+            else:
+                self.relecteurs.append(None)
+                self.redaction.append(None)
+                return False
+        else:
+            self.relecteurs.append(None)
+            self.redaction.append(None)
+            return False
+
+    # Pour la V1
+    def get_auteurs_surlignage(self, page) -> None:
+        auteurs = page.find(class_='auteur')
+        auteur = None
+
+        if auteurs is not None:
+            auteur = auteurs.text
+
+        self.auteurs.append(auteur)
 
     def get_source_surlignage(self, page) -> None:
-        source = page.find(class_='col-md-8').find('h2').find('a')
-        self.source.append(source.get('href'))
+        paragraphe = page.find(class_='col-md-8')
+        resultat = None
+        nom_source = []
+
+        if paragraphe is not None:
+            source = paragraphe.find('h2')
+            if source is not None:
+                lien = source.find('a')
+                if lien is not None:
+                    resultat = lien.get('href')
+                    liens = lien.text.split(",")
+                    for texte in liens:
+                        if not re.search(self.regex_date, texte):
+                            nom_source.append(texte)
+
+        self.nom_source.append(nom_source)
+        self.url_source.append(resultat)
 
     def get_correction_surlignage(self, page) -> None:
         correction = page.find(class_='correction')
-        self.correction.append(correction.text)
+        resultat = None
+
+        if correction is not None:
+            resultat = correction.text
+        self.correction.append(resultat)
 
     def get_contenu_surlignage(self, page) -> None:
         contenu_article = []
-        texte = page.find(class_='texte').find_all('p')
+        texte = page.find(class_='texte')
 
-        for bloc in texte[:-1]:
-            contenu_article.append(bloc.text)
+        if texte is not None:
+            paragraphe = texte.find_all('p')
+            for bloc in paragraphe[:-1]:
+                contenu_article.append(bloc.text)
 
         self.contenu.append(contenu_article)
 
+    def get_reference_surlignage(self, page) -> None:
+        liens_references = []
+        texte = page.find(class_='texte')
+
+        if texte is not None:
+            paragraphe = texte.find_all('p')
+            for bloc in paragraphe[:-1]:
+                for lien in bloc.find_all('a'):
+                    liens_references.append(lien.get('href'))
+
+        self.references.append(liens_references)
+
 
 if __name__ == '__main__':
-    url_test = "https://www.lessurligneurs.eu/philippe-ballard-conseiller-regional-veut-reinstaurer-la-primaute-du" \
-               "-droit-national-tout-en-restant-au-sein-de-lunion-europeenne/ "
+    url_test = "https://www.lessurligneurs.eu/fabien-roussel-sur-les-ecoles-hors-contrat-on-ne-sait-pas-ce-quon-y-enseigne-je-les-ferai-fermer/"
+    url_test2 = "https://www.lessurligneurs.eu/surlignage/page/1/"
 
     page_test = requests.get(url_test)
     soup_test = BeautifulSoup(page_test.content, 'lxml')
 
+    page_test2 = requests.get(url_test2)
+    soup_test2 = BeautifulSoup(page_test2.content, 'lxml')
+
     surlignage = Surlignage()
+    surlignage.get_url(soup_test2)
     surlignage.get_etiquette_surlignage(soup_test)
     surlignage.get_titre_surlignage(soup_test)
     surlignage.get_meme_theme_surlignage(soup_test)
     surlignage.get_date_surlignage(soup_test)
-    surlignage.get_contributeur_surlignage(soup_test)
+
+    if not surlignage.get_contributeur_surlignage(soup_test):
+        surlignage.get_auteurs_surlignage(soup_test)
+
     surlignage.get_source_surlignage(soup_test)
     surlignage.get_correction_surlignage(soup_test)
     surlignage.get_contenu_surlignage(soup_test)
+    surlignage.get_reference_surlignage(soup_test)
 
-    pprint(surlignage.source)
-    # pprint(surlignage.relecteurs)
-    # pprint(surlignage.redaction)
+    pprint(surlignage.etiquette)
+    pprint(surlignage.titre)
+    pprint(surlignage.meme_theme)
+    pprint(surlignage.date_creation)
+    pprint(surlignage.date_modification)
+    pprint(surlignage.auteurs)
+    pprint(surlignage.relecteurs)
+    pprint(surlignage.redaction)
+    pprint(surlignage.url_source)
+    pprint(surlignage.nom_source)
+    pprint(surlignage.references)
+    pprint(surlignage.contenu)
+
+    # pprint(surlignage.url_surlignage)
