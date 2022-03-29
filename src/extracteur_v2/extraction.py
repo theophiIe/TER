bf0ef4bfdb365bbf569bcb9f7f0c79b9b4fee07a
qgruchet <quentin.gruchet78@gmail.com>
2022-03-29 12:54:24 +0200

    Modification de la fonction get_nombre_page pour un côté générique

--- src/extracteur_v2/extraction.py
+++ src/extracteur_v2/extraction.py
@@ -1,2 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+from src.extracteur_v2.surlignage import Surlignage
+
+
+def get_nombre_pages(url) -> int:
+    numero_page = 1
+
+    while requests.get(f"{url}page/{numero_page}/").status_code != 404:
+        numero_page += 1
+
+    return numero_page - 1
+
+
+def get_nombre_articles(page) -> int:
+    articles = page.find_all(class_='grid-item')
+    return len(articles)
+
+
 if __name__ == '__main__':
+    article = Surlignage()
+
+    print(get_nombre_pages(article.url))
+    for page in range(get_nombre_pages(article.url)):
+        for article in range(get_nombre_articles(page)):
+
+
     pass
