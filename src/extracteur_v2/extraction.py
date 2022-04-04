f3f989b86489ffb7aed41d68f637ad2e350c082b
uvsq21805057 <matrixzeyphax@gmail.com>
2022-04-04 22:38:08 +0200

    Utilisation des balises du fichier json pour l'extraction

--- src/extracteur_v2/extraction.py
+++ src/extracteur_v2/extraction.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+import json
 
 from src.extracteur_v2.surlignage import Surlignage
 
@@ -17,36 +18,36 @@ def get_nombre_pages(url) -> int:
     return numero_page - 1
 
 
-def get_nombre_articles(page) -> int:
-    articles = page.find_all(class_='grid-item')
+def get_nombre_articles(page, dico_balise) -> int:
+    articles = page.find_all(class_=dico_balise['class']['container_article'])
     return len(articles)
 
 
-def get_url_all_surlignage(article) -> None:
+def get_url_all_surlignage(article, dico_balise) -> None:
     pbar = tqdm(range(1, get_nombre_pages(article.url) + 1), colour='green', desc='Obtention des urls des articles')
     for page in range(1, get_nombre_pages(article.url) + 1):
         page = requests.get(f'{article.url}/page/{page}/')
         soup = BeautifulSoup(page.content, 'lxml')
 
-        article.get_url(soup)
+        article.get_url(soup, dico_balise)
         pbar.update(1)
         pbar.refresh()
 
 
-def remplir_surlignage(surlignage) -> None:
+def remplir_surlignage(surlignage, balise) -> None:
     pbar = tqdm(range(len(surlignage.url_surlignage)), colour='green', desc='Remplissage')
     for page in surlignage.url_surlignage:
         article = requests.get(page)
         soup = BeautifulSoup(article.content, 'lxml')
-        surlignage.get_titre_surlignage(soup)
-        surlignage.get_etiquette_surlignage(soup)
-        surlignage.get_date_surlignage(soup)
-        surlignage.get_meme_theme_surlignage(soup)
-        surlignage.get_auteurs_surlignage(soup)
-        surlignage.get_source_surlignage(soup)
-        surlignage.get_contenu_surlignage(soup)
-        surlignage.get_reference_surlignage(soup)
-        surlignage.get_correction_surlignage(soup)
+        surlignage.get_titre_surlignage(soup, balise)
+        surlignage.get_etiquette_surlignage(soup, balise)
+        surlignage.get_date_surlignage(soup, balise)
+        surlignage.get_meme_theme_surlignage(soup, balise)
+        surlignage.get_auteurs_surlignage(soup, balise)
+        surlignage.get_source_surlignage(soup, balise)
+        surlignage.get_contenu_surlignage(soup, balise)
+        surlignage.get_reference_surlignage(soup, balise)
+        surlignage.get_correction_surlignage(soup, balise)
 
         pbar.update(1)
         pbar.refresh()
@@ -78,11 +79,12 @@ def debug(surlignage) -> None:
     print("Fin debug")
 
 
-
 if __name__ == '__main__':
     ar_surlignage = Surlignage()
 
-    get_url_all_surlignage(ar_surlignage)
-    remplir_surlignage(ar_surlignage)
+    with open("../../balise.json") as json_file:
+        balise = json.load(json_file)
 
+    get_url_all_surlignage(ar_surlignage, balise)
+    remplir_surlignage(ar_surlignage, balise)
     debug(ar_surlignage)
